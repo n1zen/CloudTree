9d36c1235a95a09bec6c8df7f7df128d199aff52
n1zen <114575802+n1zen@users.noreply.github.com>
2025-09-10 15:48:54 +0800

    Install lucide-react for icons and svgs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,21 @@ import {
 } from "react-native";
 
 function Header() {
-    
+
+    return(
+        <View style={styles.headerContainer}>
+            <Text>Cloud Tree</Text>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        flex: 1,
+        flexWrap: "nowrap",
+        flexShrink: 1,
+        flexBasis: "auto"
+    }
+});
 
 export default Header;
